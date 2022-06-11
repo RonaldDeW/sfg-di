8f73e97bbe00f770fa24cc80df4480fb93a2fc34
Ronald de Waard <waardder@aol.com>
2022-06-11 11:32:12 +0200

    sfg di sample

--- sfg-di/src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/28/19.
  */
-@Profile({"cat"})
-@Service("cat")
+@Profile({"CAT"})
+@Service("petService")
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
